7392399e5f0abba3ca4e316564b864cec9186fb6
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-10-07 11:09:04 +0200

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -4,7 +4,11 @@ const server = net.createServer((socket) => {
   socket.on('data', (data) => {
       const request = data.toString();
       if (request.startsWith('GET / ')) {
+        
           const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+          socket.write(httpResponse);
+      } else {
+        const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
           socket.write(httpResponse);
       }
       socket.end();
