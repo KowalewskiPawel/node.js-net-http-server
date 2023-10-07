4a4ab5b7b6396862a80d71663ded11608f9d9a1f
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-10-07 14:35:21 +0200

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -1,17 +1,34 @@
 const net = require("net");
 
+const sendBasicGetResponse = (socket) => {
+  const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+  socket.write(httpResponse);
+};
+
+const sendEchoResponse = (socket, echoString) => {
+  const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoString.length}\r\n\r\n${echoString}`;
+  socket.write(httpResponse);
+};
+
+const send404Response = (socket) => {
+  const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
+  socket.write(httpResponse);
+};
+
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-      const request = data.toString();
-      if (request.startsWith('GET / ')) {
-        
-          const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
-          socket.write(httpResponse);
-      } else {
-        const httpResponse = 'HTTP/1.1 404 NOT FOUND\r\n\r\n';
-          socket.write(httpResponse);
-      }
-      socket.end();
+    const request = data.toString();
+
+    if (request.startsWith('GET / ')) {
+      sendBasicGetResponse(socket);
+    } else if (request.startsWith('GET /echo/')) {
+      const [_, echoString] = request.match(/\/echo\/([^ ]+)/);
+      sendEchoResponse(socket, echoString);
+    }
+    else {
+      send404Response(socket);
+    }
+    socket.end();
   });
 });
 
