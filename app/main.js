de39fcea66ac9467340142bab96d6611ef9cdf0c
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-10-07 09:48:50 +0200

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,10 +1,9 @@
-const net = require("net");
+const http = require("http");
 
-const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+const server = http.createServer((_req, res) => {
+  res.end("HTTP/1.1 200 OK\r\n\r\n");
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  process.stdout.write("Listening on localhost:4221");
+});
